12a1f37bbc32f95478852f6d4591e3fcb1eb4950
Andrew Lee <andyrewlee@gmail.com>
2025-04-26 01:25:14 -0700

    Add back sign up success page since prod requires confirmation

--- components/auth/sign-up-form.tsx
+++ components/auth/sign-up-form.tsx
@@ -48,7 +48,7 @@ export function SignUpForm({
         },
       });
       if (error) throw error;
-      router.push("/");
+      router.push("/auth/sign-up-success");
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : "An error occurred");
     } finally {
